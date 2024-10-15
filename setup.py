b2a4620ec3719e402ad9ef42a1fcfb5c4227d796
Prakrit Uprety <prakritupreety0@gmail.com>
2024-10-15 12:45:14 -0400

    Add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='apple',
+    version='0.0.0',
+    author='Prakrit',
+    author_email='prakritupreety0@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
